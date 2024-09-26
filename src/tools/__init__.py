9502fb946a4ad77a83c88efc610e517f3a05ec50
Newtoneiro <latosek.bar@gmail.com>
2024-09-26 21:45:43 +0200

    add Google Face Landmark Detection model

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -2,6 +2,7 @@
 
 from tools.data_processor import DataProcessor
 from tools.frame_processors import (
+    GoogleFaceLandmarkDetection,
     SupervisionVertexProcessor,
     SupervisionEdgesProcessor,
     FrameProcessor,
